0d80e9dcc599101fb544ce1ea6215cca1aaa20e6
lukaszhajdun <lukasz.hajdun@gmail.com>
2017-09-13 20:54:05 +0200

    zadania hr.sql

--- hr.sql
+++ hr.sql
@@ -2671,4 +2671,49 @@ FROM departments d
 		LEFT JOIN employees e ON e.department_id = d.department_id
 		LEFT JOIN locations l ON d.location_id = l.location_id;
         
+#7
+SELECT first_name, last_name, salary FROM employees
+WHERE salary < (SELECT salary FROM employees WHERE employee_id =182);
 
+#WERSJA Z JOIN
+SELECT e.first_name, e.last_name, e.salary
+FROM employees e
+	JOIN employees ee on e.salary < ee.salary
+	WHERE ee.employee_id = 182;
+
+#8
+SELECT e.first_name AS 'Employee F.Name', e.last_name AS 'Employee L.Name', 
+m.first_name AS 'Boss F.Name', m.last_name AS 'Boss L.Name'
+FROM employees e
+	LEFT JOIN employees m ON e.manager_id = m.employee_id;
+
+#13
+SELECT j.job_title, d.department_name, e.first_name, e.last_name, jh.start_date
+FROM job_history jh
+	JOIN jobs j USING (job_id)
+    JOIN departments d USING (department_id)
+    JOIN employees e USING (employee_id)
+    WHERE jh.start_date >= "1993-01-01" AND jh.start_date <= "1997-07-31";
+
+#14
+SELECT country_name, city, department_name
+FROM departments
+	JOIN locations USING (location_id)
+    JOIN countries USING (country_id);
+    
+#15
+SELECT department_name, first_name, last_name
+FROM departments d
+JOIN employees e
+ON (d.manager_id = e.employee_id);
+
+#16
+SELECT country_name, city, COUNT(department_id) AS 'Ilosc departamentow'
+FROM countries
+	JOIN locations USING (country_id)
+    JOIN departments USING (location_id)
+    WHERE department_id IN
+    (SELECT department_id 
+	FROM employees
+		GROUP BY department_id HAVING COUNT(department_id) >=2) 
+        GROUP BY country_name, city;
